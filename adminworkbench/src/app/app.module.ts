81ad307cd8d22bca1ff158c773cd1e2c0b65e0b1
s0st3rUX <sophie.kim@nextcentury.com>
2017-11-17 15:46:59 -0500

    Backing up my work for the day

--- adminworkbench/src/app/app.module.ts
+++ adminworkbench/src/app/app.module.ts
@@ -3,23 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-// import { HomeComponent } from './home/home.component';
-// import { UsersComponent } from './users/users.component';
-// import { VirtuesComponent } from './virtues/virtues.component';
+import { HomeComponent } from './home/home.component';
 // import { ConfigComponent } from './config/config.component';
+// import { UsersComponent } from './users/users.component';
+import { VirtuesComponent } from './virtues/virtues.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
-    // VirtuesComponent,
-    // HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    VirtuesComponent
+    // ConfigComponent
     // UsersComponent,
     // VirtuesComponent,
-    // ConfigComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'virtues', component: VirtuesComponent }
+      // { path: virtues, component: VirtuesComponent },
+      // { path: config, component: ConfigComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
