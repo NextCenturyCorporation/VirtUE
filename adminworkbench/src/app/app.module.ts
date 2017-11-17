d4850fdb7e360d2650d196d512cc49724171df2c
s0st3rUX <sophie.kim@nextcentury.com>
2017-11-17 11:21:49 -0500

    updating the template files

--- adminworkbench/src/app/app.module.ts
+++ adminworkbench/src/app/app.module.ts
@@ -3,20 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { VirtuesComponent } from './virtues/virtues.component';
-import { ConfigComponent } from './config/config.component';
+// import { HomeComponent } from './home/home.component';
+// import { UsersComponent } from './users/users.component';
+// import { VirtuesComponent } from './virtues/virtues.component';
+// import { ConfigComponent } from './config/config.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    VirtuesComponent,
-    HomeComponent,
-    UsersComponent,
-    VirtuesComponent,
-    ConfigComponent
+    HeaderComponent
+    // VirtuesComponent,
+    // HomeComponent,
+    // UsersComponent,
+    // VirtuesComponent,
+    // ConfigComponent
   ],
   imports: [
     BrowserModule
