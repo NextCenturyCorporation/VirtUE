4f9070cb83a2a51ed62775ef34f920eb94ba1b16
Chris Long <chris.long@nextcentury.com>
2017-12-01 14:23:13 -0500

    added test of getting Kerberos credentials based on existing ones

---
+++ modules/desktop/src/test/java/com/ncc/savior/desktop/security/SignedOnUserTest.java
@@ -0,0 +1,79 @@
+package com.ncc.savior.desktop.security;
+
+import java.net.URL;
+import java.security.Principal;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+import java.util.Set;
+
+import javax.security.auth.Subject;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+
+import org.ietf.jgss.GSSContext;
+import org.ietf.jgss.GSSCredential;
+import org.ietf.jgss.GSSException;
+import org.ietf.jgss.GSSManager;
+import org.ietf.jgss.GSSName;
+import org.ietf.jgss.Oid;
+
+/**
+ * Tries to obtain a new Kerberos credential based on an existing credential.
+ *
+ * @author clong
+ *
+ */
+public class SignedOnUserTest {
+
+	public static void main(String[] args) {
+		System.setProperty("java.security.krb5.realm", "HQ.NEXTCENTURY.COM");
+		System.setProperty("java.security.krb5.kdc", "ws9.hq.nextcentury.com");
+		URL loginConfigURL = SignedOnUserTest.class.getResource("login.conf");
+		String loginConfigFilename = loginConfigURL.getFile();
+		System.setProperty("java.security.auth.login.config", loginConfigFilename);
+
+		// create a LoginContext based on the entry in the login.conf file
+		LoginContext lc;
+		try {
+			lc = new LoginContext("SignedOnUserLoginContext");
+
+			// login (effectively populating the Subject)
+			lc.login();
+
+			// get the Subject that represents the signed-on user
+			Subject signedOnUserSubject = lc.getSubject();
+			System.out.println("signed-on user subject: " + signedOnUserSubject);
+			Set<Principal> principals = signedOnUserSubject.getPrincipals();
+			System.out.println("principals: " + principals);
+
+			GSSManager manager = GSSManager.getInstance();
+
+			GSSName serverName = manager.createName("cifs@WS9", GSSName.NT_HOSTBASED_SERVICE);
+			String userNameString = principals.iterator().next().getName();
+			GSSName clientName = manager.createName(userNameString, GSSName.NT_USER_NAME);
+
+			Oid krb5Oid = new Oid("1.2.840.113554.1.2.2");
+			Subject.doAs(signedOnUserSubject, new PrivilegedExceptionAction<Object>() {
+
+				@Override
+				public Object run() throws Exception {
+					GSSCredential userCredential = manager.createCredential(clientName, 8 * 3600, (Oid) krb5Oid, GSSCredential.INITIATE_ONLY);
+					GSSContext context = manager.createContext(serverName, krb5Oid , userCredential, GSSContext.DEFAULT_LIFETIME);
+					context.requestMutualAuth(true);
+					System.out.println("successfully obtained credential based on login credential: " + context);
+					return true;
+				}
+			});
+		} catch (LoginException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GSSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (PrivilegedActionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
