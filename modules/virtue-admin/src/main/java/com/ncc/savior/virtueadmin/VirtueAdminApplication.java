be207bacfbd5f857c81bcfeb705e1653802188e8
Chris Long <chris.long@nextcentury.com>
2018-01-19 13:35:25 -0500

    updates for new spring versions

--- modules/virtue-admin/src/main/java/com/ncc/savior/virtueadmin/VirtueAdminApplication.java
+++ modules/virtue-admin/src/main/java/com/ncc/savior/virtueadmin/VirtueAdminApplication.java
@@ -1,7 +1,10 @@
 package com.ncc.savior.virtueadmin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
 
@@ -9,6 +12,7 @@ import org.springframework.context.annotation.ImportResource;
  * Applicaton Entry point
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class, SessionAutoConfiguration.class })
 @ImportResource("classpath:application-context.xml")
 public class VirtueAdminApplication {
 
