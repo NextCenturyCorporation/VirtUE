8ced62da887f6a4ecd15ca87d7e9e6d5a76e2850
s0st3rUX <sophie.kim@nextcentury.com>
2018-01-22 15:46:35 -0500

    VRTU-197: Troubleshooting and overriding  material css

--- modules/admin/src/app/virtual-machines/vm-build/vm-build.component.ts
+++ modules/admin/src/app/virtual-machines/vm-build/vm-build.component.ts
@@ -10,11 +10,11 @@ export class VmBuildComponent implements OnInit {
   osValue: string;
   osInfo: string;
   osList = [
-    { "os_name":"CentOS", "os_info":"http://mirror.centos.org/centos/7/os/x86_64/Packages/" } ,
-    { "os_name":"Debian", "os_info": "https://packages.debian.org/stable/" },
-    { "os_name":"Fedora", "os_info":"https://apps.fedoraproject.org/" },
-    { "os_name":"Red Hat Linux", "os_info":"https://access.redhat.com/downloads" },
-    { "os_name":"Windows", "os_info":"https://www.microsoft.com/en-us/windows/" }
+    { "id":10, "os_name":"CentOS", "os_info":"http://mirror.centos.org/centos/7/os/x86_64/Packages/" } ,
+    { "id":11, "os_name":"Debian", "os_info": "https://packages.debian.org/stable/" },
+    { "id":12, "os_name":"Fedora", "os_info":"https://apps.fedoraproject.org/" },
+    { "id":13, "os_name":"Red Hat Linux", "os_info":"https://access.redhat.com/downloads" },
+    { "id":14, "os_name":"Windows", "os_info":"https://www.microsoft.com/en-us/windows/" }
   ];
   constructor() { }
 
--- modules/admin/src/app/virtual-machines/vm-edit/vm-edit.component.ts
+++ modules/admin/src/app/virtual-machines/vm-edit/vm-edit.component.ts
@@ -10,7 +10,15 @@ export class VmEditComponent implements OnInit {
 
   vm: { id: number };
   vm_name: string = 'test';
-
+  osValue: string;
+  osInfo: string;
+  osList = [
+    { "id":10, "os_name":"CentOS", "os_info":"http://mirror.centos.org/centos/7/os/x86_64/Packages/" } ,
+    { "id":11, "os_name":"Debian", "os_info": "https://packages.debian.org/stable/" },
+    { "id":12, "os_name":"Fedora", "os_info":"https://apps.fedoraproject.org/" },
+    { "id":13, "os_name":"Red Hat Linux", "os_info":"https://access.redhat.com/downloads" },
+    { "id":14, "os_name":"Windows", "os_info":"https://www.microsoft.com/en-us/windows/" }
+  ];
   constructor(private router: ActivatedRoute) { }
 
   ngOnInit() {
