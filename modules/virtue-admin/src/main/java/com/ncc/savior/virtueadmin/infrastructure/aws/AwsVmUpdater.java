0f8e20d56316d59178b6c8b8b499e16d9d4d6fb4
Kyle Drumm <kyle.drumm@nextcentury.com>
2018-03-14 10:13:42 -0400

    added comment

--- modules/virtue-admin/src/main/java/com/ncc/savior/virtueadmin/infrastructure/aws/AwsVmUpdater.java
+++ modules/virtue-admin/src/main/java/com/ncc/savior/virtueadmin/infrastructure/aws/AwsVmUpdater.java
@@ -130,6 +130,17 @@ public class AwsVmUpdater {
 		executor.scheduleWithFixedDelay(getNetworkingCommand, 1000, 5000, TimeUnit.MILLISECONDS);
 	}
 
+	/**
+	 * Adds VMs to the provisioning pipeline. The provisioning pipeline leads the
+	 * VMs through a series of tasks each at individual rates. The tasks are:
+	 * <ol>
+	 * <li>Rename AWS VM
+	 * <li>Get networking information from AWS
+	 * <li>Test reachability of VM and then add unique RSA key
+	 * <li>Start Xpra server
+	 * 
+	 * @param vms
+	 */
 	public void addVmToProvisionPipeline(ArrayList<VirtualMachine> vms) {
 		logger.trace("Scheduling " + vms.size() + " vms to be renamed.");
 		for (VirtualMachine vm : vms) {
