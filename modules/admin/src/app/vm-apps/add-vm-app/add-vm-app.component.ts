93035d26712726162c18c2a65e76b3c388f00128
s0st3rUX <sophie.kim@nextcentury.com>
2018-03-16 13:42:49 -0400

    VRTU-250: Install application feature created

--- modules/admin/src/app/vm-apps/add-vm-app/add-vm-app.component.ts
+++ modules/admin/src/app/vm-apps/add-vm-app/add-vm-app.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-add-vm-app',
-  templateUrl: './add-vm-app.component.html',
-  styleUrls: ['./add-vm-app.component.css']
+  templateUrl: './add-vm-app.component.html'
 })
 export class AddVmAppComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<AddVmAppComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
 
   ngOnInit() {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
