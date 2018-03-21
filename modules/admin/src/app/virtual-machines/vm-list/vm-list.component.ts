822e905ae0960e448ac21c70614a99773471ea31
s0st3rUX <sophie.kim@nextcentury.com>
2018-03-21 13:53:10 -0400

    VRTU-239: added the enable and disable function, as well as, delete warning dialog

--- modules/admin/src/app/virtual-machines/vm-list/vm-list.component.ts
+++ modules/admin/src/app/virtual-machines/vm-list/vm-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
 import { VirtualMachineService } from '../../shared/services/vm.service';
+import { DialogsComponent } from '../../dialogs/dialogs.component';
+import { ActiveClassDirective } from '../../shared/directives/active-class.directive';
 
 @Component({
   selector: 'app-vm-list',
@@ -16,7 +19,10 @@ export class VmListComponent implements OnInit {
   vmlist: string;
   totalVms: number;
 
-  constructor( private vmService: VirtualMachineService ) { }
+  constructor(
+    private vmService: VirtualMachineService,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit() {
     this.getVmList();
@@ -36,6 +42,30 @@ export class VmListComponent implements OnInit {
     this.totalVms = this.vms.length;
   }
 
+  updateStatus(id: string): void {
+    const vm = this.vms.filter(data => data['id'] === id);
+    vm.map((_, i) => {
+      vm[i].enabled ? vm[i].enabled = false : vm[i].enabled = true;
+      console.log(vm);
+    });
+    // this.appsService.update(id, app);
+  }
+
+  openDialog(id, type, action, text): void {
+    let dialogRef = this.dialog.open(DialogsComponent, {
+      width: '450px',
+      data:  {
+          dialogText: text,
+          dialogType: type
+        }
+    });
+
+    dialogRef.updatePosition({ top: '15%', left: '36%' });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
   // addVM(name: string, status: string) {
   //   this.vms.push({name: name, status: status});
   // }
