c56adf2bee3b329ce9cbe7965eb59253b479c6de
Kyle Drumm <kyle.drumm@nextcentury.com>
2018-04-05 09:41:41 -0400

    updated to kick vms out of pipeline if AWS reports they don't exist

--- modules/virtue-admin/src/main/java/com/ncc/savior/virtueadmin/infrastructure/aws/AwsUpdateStatus.java
+++ modules/virtue-admin/src/main/java/com/ncc/savior/virtueadmin/infrastructure/aws/AwsUpdateStatus.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.concurrent.ScheduledExecutorService;
 
 import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.model.AmazonEC2Exception;
 import com.ncc.savior.virtueadmin.infrastructure.pipelining.BaseGroupedVmPipelineComponent;
 import com.ncc.savior.virtueadmin.infrastructure.pipelining.PipelineWrapper;
 import com.ncc.savior.virtueadmin.model.VirtualMachine;
@@ -23,7 +24,13 @@ public class AwsUpdateStatus extends BaseGroupedVmPipelineComponent<VirtualMachi
 
 	@Override
 	protected void onExecute(Collection<PipelineWrapper<VirtualMachine>> wrappers) {
+		try {
 		AwsUtil.updateStatusOnVms(ec2, unwrap(wrappers));
+		} catch (AmazonEC2Exception e) {
+			if (e.getErrorCode().equals("InvalidInstanceID.NotFound")) {
+				doOnFailure(wrappers);
+			}
+		}
 		Iterator<PipelineWrapper<VirtualMachine>> itr = wrappers.iterator();
 		while (itr.hasNext()) {
 			PipelineWrapper<VirtualMachine> wrapper = itr.next();
