47ec41ebafd2a20827feb5ecc4493d2e58907e2a
Kyle Drumm <kyle.drumm@nextcentury.com>
2018-05-23 13:33:08 -0400

    checking in to test on another machine

---
+++ modules/desktop/src/main/java/com/ncc/savior/desktop/clipboard/linux/ILinuxClipboardX11.java
@@ -0,0 +1,122 @@
+package com.ncc.savior.desktop.clipboard.linux;
+
+import com.sun.jna.Native;
+import com.sun.jna.NativeLong;
+import com.sun.jna.platform.unix.X11;
+
+public interface ILinuxClipboardX11 extends X11 {
+	ILinuxClipboardX11 INSTANCE = (ILinuxClipboardX11) Native.loadLibrary("X11", ILinuxClipboardX11.class);
+
+	/**
+	 * See https://tronche.com/gui/x/xlib/window-information/XGetSelectionOwner.html
+	 *
+	 * Arguments
+	 *
+	 * @param display
+	 *            Specifies the connection to the X server.
+	 * @param selection
+	 *            Specifies the selection atom whose owner you want returned.
+	 *
+	 *
+	 *            Description The XGetSelectionOwner() function returns the window
+	 *            ID associated with the window that currently owns the specified
+	 *            selection. If no selection was specified, the function returns the
+	 *            constant None . If None is returned, there is no owner for the
+	 *            selection. XGetSelectionOwner() can generate a BadAtom error.
+	 *
+	 *            Diagnostics BadAtom A value for an Atom argument does not name a
+	 *            defined Atom.
+	 *
+	 */
+	Window XGetSelectionOwner(Display display, Atom selection);
+
+	/**
+	 * Arguments
+	 *
+	 * @param display
+	 *            Specifies the connection to the X server.
+	 * @param selection
+	 *            Specifies the selection atom.
+	 * @param owner
+	 *            Specifies the owner of the specified selection atom. You can pass
+	 *            a window or None .
+	 * @param time
+	 *            Specifies the time. You can pass either a timestamp or
+	 *            CurrentTime.
+	 *
+	 *            See
+	 *            https://tronche.com/gui/x/xlib/window-information/XSetSelectionOwner.html
+	 *
+	 *            <pre>
+	 * 			The XSetSelectionOwner() function changes the owner and
+	 *            last-change time for the specified selection and has no effect if
+	 *            the specified time is earlier than the current last-change time of
+	 *            the specified selection or is later than the current X server
+	 *            time. Otherwise, the last-change time is set to the specified
+	 *            time, with CurrentTime replaced by the current server time. If the
+	 *            owner window is specified as None , then the owner of the
+	 *            selection becomes None (that is, no owner). Otherwise, the owner
+	 *            of the selection becomes the client executing the request. If the
+	 *            new owner (whether a client or None ) is not the same as the
+	 *            current owner of the selection and the current owner is not None ,
+	 *            the current owner is sent a SelectionClear event. If the client
+	 *            that is the owner of a selection is later terminated (that is, its
+	 *            connection is closed) or if the owner window it has specified in
+	 *            the request is later destroyed, the owner of the selection
+	 *            automatically reverts to None , but the last-change time is not
+	 *            affected. The selection atom is uninterpreted by the X server.
+	 *            XGetSelectionOwner() returns the owner window, which is reported
+	 *            in SelectionRequest and SelectionClear events. Selections are
+	 *            global to the X server.
+	 *
+	 *            XSetSelectionOwner() can generate BadAtom and BadWindow errors.
+	 *
+	 *            Diagnostics BadAtom A value for an Atom argument does not name a
+	 *            defined Atom. BadWindow A value for a Window argument does not
+	 *            name a defined Window.
+	 *
+	 */
+	void XSetSelectionOwner(Display display, Atom selection, Window owner, NativeLong time);
+
+	/**
+	 *
+	 * @param display
+	 *            Specifies the connection to the X server.
+	 * @param selection
+	 *            Specifies the selection atom.
+	 * @param target
+	 *            Specifies the target atom. Really the type or format of the
+	 *            selection.
+	 * @param property
+	 *            Specifies the property name. You also can pass None .
+	 * @param requestor
+	 *            Specifies the requestor.
+	 * @param time
+	 *            Specifies the time. You can pass either a timestamp or
+	 *            CurrentTime.
+	 *
+	 *            XConvertSelection() requests that the specified selection be
+	 *            converted to the specified target type:
+	 *            <ul>
+	 *            <li>If the specified selection has an owner, the X server sends a
+	 *            .PN SelectionRequest event to that owner.
+	 *            <li>If no owner for the specified selection exists, the X server
+	 *            generates a SelectionNotify event to the requestor with property
+	 *            None.
+	 *            </ul>
+	 *            The arguments are passed on unchanged in either of the events.
+	 *            There are two predefined selection atoms: PRIMARY and SECONDARY.
+	 *
+	 *            XConvertSelection() can generate BadAtom and BadWindow errors.
+	 *
+	 *            Diagnostics
+	 *            <ul>
+	 *            <li>BadAtom A value for an Atom argument does not name a defined
+	 *            Atom.
+	 *            <li>BadWindow A value for a Window argument does not name a
+	 *            defined Window.
+	 *            </ul>
+	 */
+	void XConvertSelection(Display display, Atom selection, Atom target, Atom property, Window requestor,
+			NativeLong time);
+}
---
+++ modules/desktop/src/main/java/com/ncc/savior/desktop/clipboard/linux/X11ClipboardWrapper.java
@@ -0,0 +1,152 @@
+package com.ncc.savior.desktop.clipboard.linux;
+
+import java.util.Collection;
+import java.util.Date;
+
+import com.ncc.savior.desktop.clipboard.IClipboardWrapper;
+import com.ncc.savior.desktop.clipboard.data.ClipboardData;
+import com.ncc.savior.util.JavaUtil;
+import com.sun.jna.NativeLong;
+import com.sun.jna.platform.unix.X11;
+import com.sun.jna.platform.unix.X11.Atom;
+import com.sun.jna.platform.unix.X11.Display;
+import com.sun.jna.platform.unix.X11.Window;
+import com.sun.jna.platform.unix.X11.XErrorEvent;
+import com.sun.jna.platform.unix.X11.XErrorHandler;
+import com.sun.jna.platform.unix.X11.XEvent;
+import com.sun.jna.platform.unix.X11.XSelectionClearEvent;
+import com.sun.jna.platform.unix.X11.XSelectionEvent;
+import com.sun.jna.platform.unix.X11.XSelectionRequestEvent;
+
+public class X11ClipboardWrapper implements IClipboardWrapper {
+
+	@Override
+	public void setDelayedRenderFormats(Collection<Integer> formats) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setClipboardListener(IClipboardListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setDelayedRenderData(ClipboardData clipboardData) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ClipboardData getClipboardData(int format) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		ILinuxClipboardX11 x11 = ILinuxClipboardX11.INSTANCE;
+		Display display = X11.INSTANCE.XOpenDisplay(null);
+
+		Window window = X11.INSTANCE.XDefaultRootWindow(display);
+
+		XEvent event = new XEvent();
+		// x11.XNextEvent(display, event_return);
+
+		XErrorHandler handler = new XErrorHandler() {
+
+			@Override
+			public int apply(Display display, XErrorEvent errorEvent) {
+				byte[] buffer = new byte[2048];
+				x11.XGetErrorText(display, errorEvent.error_code, buffer, 2048);
+				System.out.println(new String(buffer));
+				return 1;
+			}
+		};
+		x11.XSetErrorHandler(handler);
+
+		Atom clipboard_atom = x11.XInternAtom(display, "CLIPBOARD", false);
+		Atom utf8 = x11.XInternAtom(display, "UTF8_STRING", false);
+		Atom penguin = x11.XInternAtom(display, "Penguin", false);
+		Window retWin = x11.XGetSelectionOwner(display, clipboard_atom);
+		System.out.println("Me=" + window + " clipboard owner=" + retWin);
+
+		long currentTime = 0l;
+
+		x11.XSetSelectionOwner(display, clipboard_atom, window, new NativeLong(currentTime));
+		// retWin = x11.XGetSelectionOwner(display, clipboard_atom);
+		// System.out.println("Me=" + window + " clipboard owner=" + retWin + " time=" +
+		// currentTime);
+		// I believe XConvertSelection requests the selection
+		// XConvertSelection - requests that the specified selection be converted to the
+		// specified target type:
+		// x11.XConvertSelection(display, clipboard_atom, utf8, penguin, window, new
+		// NativeLong(0));
+		// retWin = x11.XGetSelectionOwner(display, clipboard_atom);
+		System.out.println("Me=" + window + " clipboard owner=" + retWin);
+
+		for (int i = 0; i < 5000; i++) {
+			// System.out.println("#Events: " + x11.XEventsQueued(display, 0));
+			XEvent peekEvent = new XEvent();
+			int peekRet = x11.XPeekEvent(display, peekEvent);
+			// System.out.println("Peek: "+peekRet+" "+peekEvent);
+			if (peekRet != 0) {
+				XEvent event2 = new XEvent();
+				int eventReturn = x11.XNextEvent(display, event2);
+				System.out.println("eventReturn: " + eventReturn);
+				handleEvent(x11, event2, display);
+			} else {
+			}
+			JavaUtil.sleepAndLogInterruption(100);
+		}
+	}
+
+	private static void handleEvent(ILinuxClipboardX11 x11, XEvent event, Display display) {
+		System.out.println("eventType=" + event.type);
+		switch (event.type) {
+		case X11.SelectionRequest:
+			XSelectionRequestEvent sre = (XSelectionRequestEvent) XEvent.newInstance(XSelectionRequestEvent.class,
+					event.getPointer());
+			sre.autoRead();
+			XSelectionEvent se = new X11.XSelectionEvent();
+			se.display = sre.display;
+			se.property = sre.property;
+			se.requestor = sre.requestor;
+			// selection or clipboard name (clipboard)
+			se.selection = sre.selection;
+			// type or format
+			se.target = sre.target;
+			Atom penguin = x11.XInternAtom(display, "Penguin", false);
+			se.property = penguin;
+			se.type = X11.SelectionNotify;
+			int propagate = 0;
+			XEvent sendEvent = new XEvent();
+			se.autoWrite();
+			sendEvent.setTypedValue(se);
+			int sendRet = x11.XSendEvent(display, sre.requestor, propagate, new NativeLong(X11.NoEventMask), sendEvent);
+			System.out.println("SelectionRequestEvent received.  Sent event with ret=" + sendRet);
+			break;
+		case X11.SelectionClear:
+			XSelectionClearEvent sce = (XSelectionClearEvent) XEvent.newInstance(XSelectionClearEvent.class,
+					event.getPointer());
+			System.out.println("selection cleared " + sce);
+			break;
+		case X11.SelectionNotify:
+			XSelectionEvent sne = (XSelectionEvent) XEvent.newInstance(XSelectionEvent.class, event.getPointer());
+			System.out.println("selection notified " + sne);
+			break;
+		default:
+			System.out.println("got event " + event.type);
+		}
+
+	}
+
+	private static NativeLong getNow() {
+		return new NativeLong(new Date().getTime());
+	}
+
+	private static NativeLong getNow(int i) {
+		return new NativeLong(new Date().getTime() + i);
+	}
+
+}
