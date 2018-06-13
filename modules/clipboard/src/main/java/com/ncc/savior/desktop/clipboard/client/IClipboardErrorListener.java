fa130b20b002a7448fc91ea6ae433e130d2dc37f
Kyle Drumm <kyle.drumm@nextcentury.com>
2018-06-13 11:49:40 -0400

    updated error handling and restarting clipboard

---
+++ modules/clipboard/src/main/java/com/ncc/savior/desktop/clipboard/client/IClipboardErrorListener.java
@@ -0,0 +1,13 @@
+package com.ncc.savior.desktop.clipboard.client;
+
+/**
+ * Indicates that an error has occurred and the clipboard client is no longer
+ * working.
+ * 
+ *
+ */
+public interface IClipboardErrorListener {
+
+	void onError();
+
+}
