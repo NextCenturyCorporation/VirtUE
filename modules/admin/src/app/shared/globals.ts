90399667f4ef0dbb7a4459422dcd4a615f4966b6
s0st3rUX <sophie.kim@nextcentury.com>
2018-06-21 12:19:20 -0400

    VRTU-405: updated files affected by update to angular 6

--- modules/admin/src/app/shared/globals.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-
-export class Globals {
-  serverUrl: string = 'http://ec2-34-207-150-143.compute-1.amazonaws.com:8080';
-}
---
+++ modules/admin/src/app/shared/providers/breadcrumb.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Subject } from 'rxjs';
+
+import { Breadcrumb } from '../models/breadcrumb.model';
+
+@Injectable()
+export class BreadcrumbProvider {
+
+  _addItem = new Subject<Breadcrumb>();
+
+  constructor(private router: Router) { }
+
+  addItem(label: string, href: string = this.router.url): void {
+    this._addItem.next(new Breadcrumb(label, href));
+  }
+
+}
