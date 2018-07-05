7fef8757201183805faee876b332cfae3a82e7ec
Chris Long <chris.long@nextcentury.com>
2018-07-05 13:59:51 -0400

    can get a service ticket if we already have a TGT

--- modules/ecosystem/cifs-proxy/src/cpp/getserviceticket.cpp
+++ modules/ecosystem/cifs-proxy/src/cpp/getserviceticket.cpp
@@ -10,60 +10,87 @@
 #include <krb5/krb5.h>
 #include <cstring>
 
+template < typename T >
+class auto_free {
+public:
+	typedef void (*FreeFunc)(T);
+	auto_free(FreeFunc freeFunc)
+	: _freeFunc(freeFunc)
+	{
+	}
+	~auto_free() {
+		_freeFunc(_value);
+	}
+	T& operator*() {
+		return _value;
+	}
+private:
+	FreeFunc _freeFunc;
+	T _value;
+};
+
 krb5_error_code getServiceTicket(const char* inCacheFilename,
-		const char* serviceName, const char* outCacheFilename) {
-	krb5_context context;
+		const char* serviceName, krb5_creds* outCreds) {
+	auto_free<krb5_context> context(krb5_free_context);
 	krb5_error_code ret = 0;
 
-    ret = krb5_init_context(&context);
+    ret = krb5_init_context(&*context);
     if (ret) {
         return ret;
     }
 
-    ret = krb5_cc_set_default_name(context, inCacheFilename);
+    ret = krb5_cc_set_default_name(*context, inCacheFilename);
     if (ret) {
         return ret;
     }
 	krb5_ccache cache;
-	krb5_principal princ;
+    ret = krb5_cc_default(*context, &cache);
+    if (ret) {
+        return ret;
+    }
 
-    ret = krb5_cc_get_principal(context, cache, &princ);
-    krb5_cc_cursor cur;
-	ret = krb5_cc_start_seq_get(context, cache, &cur);
+	krb5_principal princ = NULL;
+    ret = krb5_cc_get_principal(*context, cache, &princ);
 	if (ret) {
 		return ret;
 	}
-	krb5_creds creds;
-	while ((ret = krb5_cc_next_cred(context, cache, &cur, &creds)) == 0) {
-		krb5_creds* outCreds;
-		krb5_get_credentials(context, 0, cache, &creds, &outCreds);
-		krb5_free_cred_contents(context, &creds);
-	}
-	krb5_free_principal(context, princ);
 
-	if (ret == KRB5_CC_END) {
-		ret = krb5_cc_end_seq_get(context, cache, &cur);
-	}
+	krb5_creds inCreds;
+
+    memset(&inCreds, 0, sizeof(inCreds));
+
+	inCreds.client = princ;
+    ret = krb5_parse_name(*context, serviceName, &inCreds.server);
+    if (ret) {
+    	krb5_free_principal(*context, princ);
+    	return ret;
+    }
+
+	ret = krb5_get_credentials(*context, 0, cache, &inCreds, &outCreds);
+
+	krb5_free_principal(*context, princ);
+	krb5_free_principal(*context, inCreds.server);
 
 	return ret;
 }
 
 int main(int argc, const char* argv[]) {
-	if (argc != 4) {
+	if (argc != 3) {
 		std::cerr << "error: usage: "
 				<< argv[0]
 				<< " <input-cache-file>"
 				<< " <service-name>"
-				<< " <output-cache-file>"
 				<< std::endl;
 		return -1;
 	}
 	int i = 1;
 	const char* inCacheFilename = argv[i++];
 	const char* serviceName = argv[i++];
-	const char* outCacheFilename = argv[i++];
-
-	getServiceTicket(inCacheFilename, serviceName, outCacheFilename);
-
-	return 0;
+	krb5_creds* creds = NULL;
+	krb5_error_code ret;
+	ret = getServiceTicket(inCacheFilename, serviceName, creds);
+	if (ret) {
+		std::cerr << "error getting service ticket: " << ret << std::endl;
+	}
+	return ret;
 }
