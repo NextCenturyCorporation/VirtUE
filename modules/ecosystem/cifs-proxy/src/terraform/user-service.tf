0e0f824411f4eb8a87ff87875e813afac0c65c41
Chris Long <chris.long@nextcentury.com>
2018-08-01 19:27:53 -0400

    Revert "switched user-service vm to Ubuntu"
    This reverts commit 22a8793cfb47a4fc31609c4458261ad94f91dbb7.
    
    Switched to ubuntu for samba-tools, but it only works on the local
    machine, so reverting back to Amazon-provided linux.

--- modules/ecosystem/cifs-proxy/src/terraform/user-service.tf
+++ modules/ecosystem/cifs-proxy/src/terraform/user-service.tf
@@ -32,20 +32,18 @@ resource "aws_instance" "user_facing_server" {
 set -x
 exec > /var/log/user_data.log 2>&1
 date
-export DEBIAN_FRONTEND=noninteractive
-echo krb5-config krb5-config/default_realm string TEST.SAVIOR | sudo debconf-set-selections
-echo krb5-config krb5-config/add_servers_realm string TEST.SAVIOR | sudo debconf-set-selections
-apt-get update && \
-apt -y install sssd realmd krb5-user libpam-krb5 libpam-ccreds auth-client-config samba-common packagekit adcli sssd-tools libnss-sss libpam-sss
+yum -y update && yum -y install sssd realmd krb5-workstation samba-common-tools
 hostnamectl set-hostname ${local.myname}.${var.domain}
 sed -i 's/\(^127\.0\.0\.1 *\)/\1${local.myname} ${local.myname}.${var.domain} /' /etc/hosts
 (echo supersede domain-name-servers "${aws_directory_service_directory.active_directory.dns_ip_addresses[0]}", "${aws_directory_service_directory.active_directory.dns_ip_addresses[1]}" ';'
 echo supersede domain-search \"${var.domain}\";
 echo supersede domain-name \"${var.domain}\";
 ) >> /etc/dhcp/dhclient.conf
-systemctl restart networking.service
-sed -i 's/^\(\[libdefaults\]\)/\1\n  rdns = false/' /etc/krb5.conf
+systemctl restart network.service
 echo ${var.admin_password} | realm join --user Admin ${var.domain}
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+systemctl restart sshd.service
+systemctl start sssd.service
 date
 EOF
 
