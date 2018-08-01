21afc0ea69bfc49327ef77dd5992adc90e343411
Kyle Drumm <kyle.drumm@nextcentury.com>
2018-08-01 10:29:13 -0400

    fixed mispelling

--- modules/common/src/main/java/com/ncc/savior/configuration/PropertyManager.java
+++ modules/common/src/main/java/com/ncc/savior/configuration/PropertyManager.java
@@ -63,7 +63,7 @@ public class PropertyManager {
 	}
 
 	public PropertyManager(String... resourcePaths) {
-		this(resourcePathesToStreams(resourcePaths));
+		this(resourcePathsToStreams(resourcePaths));
 	}
 
 	private static List<InputStream> filesToStreams(File[] files) {
@@ -82,7 +82,7 @@ public class PropertyManager {
 		return list;
 	}
 
-	private static List<InputStream> resourcePathesToStreams(String[] resourcePaths) {
+	private static List<InputStream> resourcePathsToStreams(String[] resourcePaths) {
 		PathMatchingResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
 		List<InputStream> list = new ArrayList<InputStream>();
 		for (String path : resourcePaths) {
