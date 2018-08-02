44d8cfaecf1474387802474492190668d531d8b8
Kyle Drumm <kyle.drumm@nextcentury.com>
2018-08-02 11:35:34 -0400

    added files that were accidentally ignored

---
+++ modules/clipboard/src/main/java/com/ncc/savior/desktop/alerting/BaseAlertMessage.java
@@ -0,0 +1,26 @@
+package com.ncc.savior.desktop.alerting;
+
+/**
+ * Base class for alert messages that handles some common functionality such as
+ * storing the time the alert was created.
+ * 
+ */
+public abstract class BaseAlertMessage {
+	protected long timeMillis;
+	protected String title;
+
+	protected BaseAlertMessage(String title) {
+		this.title = title;
+		this.timeMillis = System.currentTimeMillis();
+	}
+
+	public long getTime() {
+		return timeMillis;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public abstract String getPlainTextMessage();
+}
