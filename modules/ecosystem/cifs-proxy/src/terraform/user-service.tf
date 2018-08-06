b8a0440e9cc055f3a3c0e4fd39a44a7d5f87bf58
Chris Long <chris.long@nextcentury.com>
2018-08-06 15:00:46 -0400

    added Java, fixed smb.conf fix
    Apparently, can't nest here doc in terraform here doc :(

--- modules/ecosystem/cifs-proxy/src/terraform/user-service.tf
+++ modules/ecosystem/cifs-proxy/src/terraform/user-service.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "user_facing_server" {
 set -x
 exec > /var/log/user_data.log 2>&1
 date
-yum -y update && yum -y install sssd realmd krb5-workstation samba-common-tools
+yum -y update && yum -y install sssd realmd krb5-workstation samba-common-tools java-1.8.0-openjdk
 hostnamectl set-hostname ${local.myname}.${var.domain}
 sed -i 's/\(^127\.0\.0\.1 *\)/\1${local.myname}.${var.domain} ${local.myname} /' /etc/hosts
 (echo supersede domain-name-servers "${aws_directory_service_directory.active_directory.dns_ip_addresses[0]}", "${aws_directory_service_directory.active_directory.dns_ip_addresses[1]}" ';'
@@ -41,11 +41,11 @@ echo supersede domain-name \"${var.domain}\";
 ) >> /etc/dhcp/dhclient.conf
 systemctl restart network.service
 echo ${var.admin_password} | realm join --user Admin ${var.domain}
-cat >> /etc/samba/smb.conf <<_EOF
-security = ads
-realm = ${var.domain}
-workgroup = test
-_EOF
+(
+echo security = ads
+echo realm = ${var.domain}
+echo workgroup = test
+) >> /etc/samba/smb.conf
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 systemctl restart sshd.service
 systemctl start sssd.service
@@ -53,10 +53,5 @@ echo '${var.admin_password}' | sudo net -k ads keytab add HTTP -U Admin
 date
 EOF
 
-  provisioner "file" {
-	source = "cifs-proxy-server-0.0.1.jar"
-	destination = "/usr/local/lib"
-  }
-  
   depends_on = [ "aws_directory_service_directory.active_directory" ]
 }
