98ef19099b052415bb9694ec3b766b7bdab2b8e3
Chris Hill <hill.c.james@gmail.com>
2018-08-22 16:59:45 -0400

    stashing initial changes

--- modules/common/src/main/java/com/ncc/savior/virtueadmin/model/VirtueTemplate.java
+++ modules/common/src/main/java/com/ncc/savior/virtueadmin/model/VirtueTemplate.java
@@ -19,7 +19,7 @@ import com.fasterxml.jackson.annotation.JsonSetter;
 
 /**
  * Data Transfer Object (DTO) for templates.
- * 
+ *
  *
  */
 @Entity
@@ -34,11 +34,13 @@ public class VirtueTemplate {
 	private boolean enabled;
 	private Date lastModification;
 	private String lastEditor;
-	
+
 	private String userCreatedBy;
 	private Date timeCreatedAt;
 
 	private String awsTemplateName;
+
+	private String color;
 
 	@Transient
 	private Collection<String> virtualMachineTemplateIds;
@@ -47,7 +49,7 @@ public class VirtueTemplate {
 
 	/**
 	 * Used for jackson deserialization
-	 * 
+	 *
 	 * @param template
 	 * @param templateId
 	 */
@@ -57,10 +59,26 @@ public class VirtueTemplate {
 		this.name = template.getName();
 		this.version = template.getVersion();
 		this.vmTemplates = template.getVmTemplates();
+		this.color = template.color();
 		this.enabled = template.isEnabled();
 		this.lastModification = template.getLastModification();
 		this.lastEditor = template.getLastEditor();
 		this.awsTemplateName = template.getAwsTemplateName();
+		this.awsTemplateName = template.getAwsTemplateName();
+	}
+
+	public VirtueTemplate(String id, String name, String version, Collection<VirtualMachineTemplate> vmTemplates,
+			String awsTemplateName, String color, boolean enabled, Date lastModification, String lastEditor) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.version = version;
+		this.vmTemplates = vmTemplates;
+		this.color = color;
+		this.enabled = enabled;
+		this.lastModification = lastModification;
+		this.lastEditor = lastEditor;
+		this.awsTemplateName = awsTemplateName;
 	}
 
 	public VirtueTemplate(String id, String name, String version, Collection<VirtualMachineTemplate> vmTemplates,
@@ -70,6 +88,7 @@ public class VirtueTemplate {
 		this.name = name;
 		this.version = version;
 		this.vmTemplates = vmTemplates;
+		this.color = "transparent";
 		this.enabled = enabled;
 		this.lastModification = lastModification;
 		this.lastEditor = lastEditor;
@@ -77,20 +96,21 @@ public class VirtueTemplate {
 	}
 
 	public VirtueTemplate(String id, String name, String version, VirtualMachineTemplate vmTemplate,
-			String awsTemplateName, boolean enabled, Date lastModification, String lastEditor) {
+			String awsTemplateName, String color, boolean enabled, Date lastModification, String lastEditor) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.version = version;
 		this.vmTemplates = new ArrayList<VirtualMachineTemplate>();
 		vmTemplates.add(vmTemplate);
+		this.color = color;
 		this.enabled = enabled;
 		this.lastModification = lastModification;
 		this.lastEditor = lastEditor;
 		this.awsTemplateName = awsTemplateName;
 	}
 
-	public VirtueTemplate(String id, String name, String version, String awsTemplateName, boolean enabled,
+	public VirtueTemplate(String id, String name, String version, String awsTemplateName, String color, boolean enabled,
 			Date lastModification, String lastEditor, VirtualMachineTemplate... vmTemplates) {
 		super();
 		this.id = id;
@@ -100,19 +120,21 @@ public class VirtueTemplate {
 		for (VirtualMachineTemplate vmTemplate : vmTemplates) {
 			this.vmTemplates.add(vmTemplate);
 		}
+		this.color = color;
 		this.enabled = enabled;
 		this.lastModification = lastModification;
 		this.lastEditor = lastEditor;
 		this.awsTemplateName = awsTemplateName;
 	}
-	
+
 	public VirtueTemplate(String id, String name, String version, Collection<VirtualMachineTemplate> vmTemplates,
-			String awsTemplateName, boolean enabled, Date lastModification, String lastEditor, String userCreatedBy, Date timeCreatedAt) {
+			String awsTemplateName, String color, boolean enabled, Date lastModification, String lastEditor, String userCreatedBy, Date timeCreatedAt) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.version = version;
 		this.vmTemplates = vmTemplates;
+		this.color = color;
 		this.enabled = enabled;
 		this.lastModification = lastModification;
 		this.lastEditor = lastEditor;
@@ -178,6 +200,10 @@ public class VirtueTemplate {
 
 	public void setAwsTemplateName(String awsTemplateName) {
 		this.awsTemplateName = awsTemplateName;
+	}
+
+	public boolean color() {
+		return color;
 	}
 
 	public boolean isEnabled() {
