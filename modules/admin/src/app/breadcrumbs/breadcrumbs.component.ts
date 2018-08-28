77ec80547eb77ecc5dedad5577d4eb79efc1d512
Chris Hill <hill.c.james@gmail.com>
2018-08-28 10:53:23 -0400

    Changes before code review

--- modules/admin/src/app/breadcrumbs/breadcrumbs.component.ts
+++ modules/admin/src/app/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivationEnd, NavigationEnd, ActivatedRoute, RouterModule, Router } from '@angular/router';
+import { ActivationEnd, NavigationStart, ActivatedRoute, RouterModule, Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -25,19 +25,21 @@ export class BreadcrumbsComponent implements OnInit {
     let tempCrumbs: Breadcrumb[] = [];
     this.routerSub = this.router.events.subscribe(
       e => {
-      if (e instanceof ActivationEnd) {
+      // This function gets called many times on each navigation.
+
+      // NavigationStart appears to be one of the first events in the chain that get
+      // get fired off when you navigate to a new link. So when you see that event, reset
+      // tempCrumbs, so the breadcrumb list starts with a clean slate.
+      if (e instanceof NavigationStart) {
+        tempCrumbs = [];
+      }
+      // ActivationEnd events occur once for every page that was hit while navigating to
+      // this one. For each such event, add a link to the corresponding page.
+      else if (e instanceof ActivationEnd) {
         if (e.snapshot.data.breadcrumb) {
           tempCrumbs.unshift(e.snapshot.data.breadcrumb);
         }
         this.breadcrumbs = tempCrumbs;
-      }
-
-      // ugly
-      // Navigation end appears to be one of the last events in the chain that get
-      // get fired off when you navigate to a new link. So when you see that, reset
-      // tempCrumbs, so the next navigation starts with a clean slate.
-      if (e instanceof NavigationEnd) {
-        tempCrumbs = [];
       }
     });
   }
