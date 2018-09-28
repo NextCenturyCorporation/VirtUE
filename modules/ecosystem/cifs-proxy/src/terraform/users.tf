ea6bba463dfe379c3ef0c65b0eccc21ca50e5180
Chris Long <chris.long@nextcentury.com>
2018-09-27 20:57:41 -0400

    migrated user creation from Microsoft AD to samba

--- modules/ecosystem/cifs-proxy/src/terraform/users.tf
+++ modules/ecosystem/cifs-proxy/src/terraform/users.tf
@@ -2,60 +2,25 @@
 # Create users in our AD domain
 #
 
-data "template_file" "users_script" {
-  template = "${file("users.ps1")}"
-
-  vars {
-	admin_password = "${var.admin_password}"
-	bob_password = "${var.bob_password}"
-  }
-}
-
-data "template_file" "remove_users_script" {
-  template = "${file("users-remove.ps1")}"
-
-  vars {
-	admin_password = "${var.admin_password}"
-  }
-}
-
 resource "null_resource" "user_creation" {
-  triggers {
-	script_sha1 = "${sha1(data.template_file.users_script.template)}"
-	# Not depending on the remove script because it should only change
-	# when the add script does.
-  }
 
   connection {
-	user = "Administrator"
-	password = "${var.admin_password}"
-	host = "${aws_instance.file_server.public_ip}"
-	type = "winrm"
-	https = false
-	use_ntlm = false
-  }
-  
-  provisioner "file" {
-	content = "${data.template_file.users_script.rendered}"
-	destination = "\\temp\\users.ps1"
+	user = "fedora"
+	host = "${aws_instance.user_facing_server.public_ip}"
+	type = "ssh"
+	private_key = "${file(var.user_private_key_file)}"
   }
   
   provisioner "remote-exec" {
 	inline = [
-	  "powershell \\temp\\users.ps1"
+	  "sudo samba-tool user create bob ${bob_password}"
 	]
   }
 
-  provisioner "file" {
-	when = "destroy"
-	content = "${data.template_file.remove_users_script.rendered}"
-	destination = "\\temp\\users-remove.ps1"
-  }
-  
   provisioner "remote-exec" {
 	when = "destroy"
 	inline = [
-	  "powershell \\temp\\users-remove.ps1"
+	  "sudo samba-tool user delete bob"
 	]
   }
 }
