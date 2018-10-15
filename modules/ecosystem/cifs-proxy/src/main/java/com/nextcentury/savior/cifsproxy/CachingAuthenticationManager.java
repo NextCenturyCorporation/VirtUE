9358d3ef520d8cc1116c13b7e4fa20cbca4e2542
Chris Long <chris.long@nextcentury.com>
2018-10-15 14:08:45 -0400

    comments for historical interest

--- modules/ecosystem/cifs-proxy/src/main/java/com/nextcentury/savior/cifsproxy/CachingAuthenticationManager.java
+++ modules/ecosystem/cifs-proxy/src/main/java/com/nextcentury/savior/cifsproxy/CachingAuthenticationManager.java
@@ -16,6 +16,7 @@ import com.nextcentury.savior.cifsproxy.GssApi.GssCredentialUsage;
 import com.nextcentury.savior.cifsproxy.GssApi.gss_OID_desc;
 import com.nextcentury.savior.cifsproxy.GssApi.gss_OID_set_desc;
 import com.nextcentury.savior.cifsproxy.GssApi.gss_buffer_desc;
+import com.nextcentury.savior.cifsproxy.GssApi.gss_cred_id_t;
 import com.nextcentury.savior.cifsproxy.GssApi.gss_key_value_element;
 import com.nextcentury.savior.cifsproxy.GssApi.gss_key_value_set;
 import com.nextcentury.savior.cifsproxy.GssApi.gss_name_t;
@@ -26,7 +27,9 @@ import com.sun.jna.ptr.IntByReference;
 import com.sun.jna.ptr.PointerByReference;
 
 /**
- * Cache the user credentials passed to us.
+ * Wraps a real {@link AuthenticationManager} and caches the user credentials
+ * passed to us, so they can be used later to mount the filesystem with
+ * cifs.mount(8).
  * 
  * @author clong
  *
@@ -36,6 +39,15 @@ public class CachingAuthenticationManager implements AuthenticationManager {
 	private AuthenticationManager amDelegate;
 	private Path cacheFile;
 
+	/**
+	 * 
+	 * @param amDelegate
+	 *                       The actual {@link AuthenticationManager}
+	 * @param cacheFile
+	 *                       where to store the credentials (in Kerberos credential
+	 *                       cache format). This file will be overwritten if it
+	 *                       exists.
+	 */
 	public CachingAuthenticationManager(AuthenticationManager amDelegate, Path cacheFile) {
 		this.amDelegate = amDelegate;
 		this.cacheFile = cacheFile;
@@ -59,29 +71,50 @@ public class CachingAuthenticationManager implements AuthenticationManager {
 							+ authentication.getClass().getName() + ")");
 		}
 		KerberosServiceRequestToken kerberosToken = (KerberosServiceRequestToken) authentication;
-		byte[] serviceTicket = kerberosToken.getToken();
+		byte[] serviceToken = kerberosToken.getToken();
 		try {
-			cacheServiceTicket(serviceTicket);
+			cacheServiceTicket(serviceToken);
 		} catch (GSSException e) {
 			throw new AuthenticationServiceException("error caching service ticket", e);
 		}
 		return amDelegate.authenticate(authentication);
 	}
 
-	private void cacheServiceTicket(byte[] serviceTicket) throws GSSException {
+	/**
+	 * Create a service ticket for ourselves and cache it for later use.
+	 * 
+	 * @param serviceToken
+	 *                         token from service authentication initiation (e.g.,
+	 *                         sent with gss_sec_init_context)
+	 * @throws GSSException
+	 *                          if the ticket could not be created or cached
+	 */
+	private void cacheServiceTicket(byte[] serviceToken) throws GSSException {
 		// maybe disable this for production
 		Native.setProtected(true);
 
 		GssApi gssapi = GssApi.INSTANCE;
 
-		IntByReference minorStatus = new IntByReference();
-		gss_buffer_desc gssToken = new gss_buffer_desc(serviceTicket);
+		gss_buffer_desc gssToken = new gss_buffer_desc(serviceToken);
 		acceptSecContext(gssapi, gssToken);
 
-		Pointer acquiredCred = acquireCred(gssapi, minorStatus);
-		storeCredInto(gssapi, minorStatus, acquiredCred, cacheFile);
+		Pointer acquiredCred = acquireCred(gssapi);
+		storeCredInto(gssapi, acquiredCred, cacheFile);
 	}
 
+	/**
+	 * Use
+	 * {@link GssApi#gss_accept_sec_context(IntByReference, PointerByReference, com.nextcentury.savior.cifsproxy.GssApi.gss_cred_id_t, gss_buffer_desc, com.nextcentury.savior.cifsproxy.GssApi.gss_channel_bindings_struct, PointerByReference, PointerByReference, gss_buffer_desc, IntByReference, IntByReference, PointerByReference)}
+	 * to get a service ticket into the default cache.
+	 * 
+	 * @param gssapi
+	 *                        the api instance to use
+	 * @param clientToken
+	 *                        the raw Kerberos token
+	 * @throws GSSException
+	 *                          if there was an error calling
+	 *                          {@link GssApi#gss_accept_sec_context(IntByReference, PointerByReference, com.nextcentury.savior.cifsproxy.GssApi.gss_cred_id_t, gss_buffer_desc, com.nextcentury.savior.cifsproxy.GssApi.gss_channel_bindings_struct, PointerByReference, PointerByReference, gss_buffer_desc, IntByReference, IntByReference, PointerByReference)}
+	 */
 	private static void acceptSecContext(GssApi gssapi, gss_buffer_desc clientToken) throws GSSException {
 		IntByReference minorStatus = new IntByReference();
 		PointerByReference contextHandle = new PointerByReference(GssApi.GSS_C_NO_CONTEXT);
@@ -100,7 +133,19 @@ public class CachingAuthenticationManager implements AuthenticationManager {
 		}
 	}
 
-	private static Pointer acquireCred(GssApi gssapi, IntByReference minorStatus) throws GSSException {
+	/**
+	 * Get the Kerberos delegated credential from the default cache.
+	 * 
+	 * @param gssapi
+	 *                   the api instance to use
+	 * @return the delegated Kerberos credential, as a {@link Pointer} to a
+	 *         {@link gss_cred_id_t}
+	 * @throws GSSException
+	 *                          if there was an error calling
+	 *                          {@link GssApi#gss_acquire_cred(IntByReference, gss_name_t, int, gss_OID_set_desc, int, PointerByReference, PointerByReference, IntByReference)}
+	 */
+	private static Pointer acquireCred(GssApi gssapi) throws GSSException {
+		IntByReference minorStatus = new IntByReference();
 		gss_name_t desiredName = GssApi.GSS_C_NO_NAME;
 		gss_OID_set_desc desiredMechs = new gss_OID_set_desc();
 		desiredMechs.count = new NativeLong(1);
@@ -124,12 +169,25 @@ public class CachingAuthenticationManager implements AuthenticationManager {
 		return acquiredCredHandle.getValue();
 	}
 
-	private static void storeCredInto(GssApi gssapi, IntByReference minorStatus, Pointer acquiredCred, Path file)
-			throws GSSException {
+	/**
+	 * Take a credential and store it in the specified file, overwriting any
+	 * preexisting file.
+	 * 
+	 * @param gssapi
+	 *                         the api instance to use
+	 * @param acquiredCred
+	 *                         the credential to store
+	 * @param file
+	 *                         the path to the file to store the credential in
+	 * @throws GSSException
+	 *                          if there was an error calling
+	 *                          {@link GssApi#gss_store_cred_into(IntByReference, Pointer, int, gss_OID_desc, int, int, gss_key_value_set, PointerByReference, IntByReference)}
+	 */
+	private static void storeCredInto(GssApi gssapi, Pointer acquiredCred, Path file) throws GSSException {
+		IntByReference minorStatus = new IntByReference();
 		int overwriteCred = 1;
 		int defaultCred = 0;
-		gss_key_value_element.ByReference credElement = new gss_key_value_element.ByReference("ccache",
-				"FILE:" + file);
+		gss_key_value_element.ByReference credElement = new gss_key_value_element.ByReference("ccache", "FILE:" + file);
 		gss_key_value_set credStore = new gss_key_value_set();
 		credStore.count = 1;
 		credStore.elements = credElement;
