1aad32a781eace331f20ed7fbf640538eade7ffc
Christopher Hill <hillcjames@users.noreply.github.com>
2018-10-25 11:19:41 -0400

    remove extraneous escape

--- modules/admin/src/app/virtues/protocols.enum.ts
+++ modules/admin/src/app/virtues/protocols.enum.ts
@@ -4,7 +4,7 @@
  * The available protocols which a virtue can be permitted to use within a given [[NetworkPermission]]
  */
 export enum NetworkProtocols {
-  TCPIP = "TCP\/IP",
+  TCPIP = "TCP/IP",
   UDP = "UDP/IP",
   ICMP = "ICMP"
 }
