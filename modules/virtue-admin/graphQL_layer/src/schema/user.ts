48a5d2896189faa62bab16792c2876537ae51926
Chris Hill <hill.c.james@gmail.com>
2018-10-25 11:25:29 -0400

    almost-proof-of-concept graphql server running

--- modules/virtue-admin/graphQL_layer/src/schema/user.ts
+++ modules/virtue-admin/graphQL_layer/src/schema/user.ts
@@ -3,8 +3,8 @@
 
 export const typeDef = `
   extend type Query {
-    users: [User]
     user(id: Int!): User
+    users: [User]
   }
   extend type Mutation {
     addUser(id: Int!, username: String!): User
@@ -22,25 +22,23 @@ type User = {id: number, username: string, virtues: Virtue[]};
 let users: User[] = [{id: 34, username: "Bob", virtues: [{id: 1, name: "V1"}, {id: 2, name: "V2"}]}];
 
 function getUsers(): User[] {
-  console.log(users);
   return users;
 }
 
 function getUser(id: number): User {
-  console.log(id, users[0].id, id === users[0].id);
   return users.find(x => x.id === id);
 }
 
-function addUser(newId: number, newUsername: string): User {
-  users.push({id:newId, username: newUsername, virtues: [{id: 0, name: "V0"}]});
+function addUser(newId: number, username: string): User {
+  users.push({id:newId, username: username, virtues: [{id: 0, name: "V0"}]});
   return users[users.length - 1];
 }
 
-function changeName(id: number, newUsername: string): User {
+function changeName(id: number, newName: string): User {
   let user = getUser(id);
   if (user !== undefined) {
-    user.username = newUsername;
-    console.log("changing name to ", newUsername);
+    user.username = newName;
+    console.log("changing name to ", user.username);
     return user;
   }
 }
@@ -48,14 +46,13 @@ function changeName(id: number, newUsername: string): User {
 export const resolvers = {
   Query: {
     users: () => getUsers(),
-    // users: () => users,
-    user: (id: number) => {console.log(id); return getUser(id)},
+    user: (_: any, input: {id: number}) => getUser(input.id),
   },
   Mutation: {
-    addUser: (id: number, username: string) => addUser(id, username),
-    changeName: (id: number, newUsername: string) => changeName(id, newUsername)
-  }//,
-  // User: {
-  //   name: () => username,
-  // }
+    addUser: (_: any, input: {id: number, username: string}) => addUser(input.id, input.username),
+    changeName: (_: any, input: {id: number, newName: string}) => changeName(input.id, input.newName)
+  },
+  User: {
+    name: () => username,
+  }
 };
