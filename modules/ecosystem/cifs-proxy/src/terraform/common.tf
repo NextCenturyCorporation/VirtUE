3b66b4d7a0cc0e3518984f275e26143a78caf66a
Chris Long <chris.long@nextcentury.com>
2018-10-29 17:48:55 -0400

    basic samba server configuration

--- modules/ecosystem/cifs-proxy/src/terraform/common.tf
+++ modules/ecosystem/cifs-proxy/src/terraform/common.tf
@@ -32,6 +32,12 @@ variable "user_private_key_file" {
   default = "vrtu.pem"
 }
 
+variable "linux_user" {
+  # The user name to use to ssh into a machine we create (as a normal user)
+  # Change this if the Linux AMI distro changes
+  default = "fedora"
+}
+
 variable "bob_password" {
   default = "Test1234."
 }
@@ -76,6 +82,7 @@ variable "linux_ami" {
   # Using Fedora 28 because it has more recent version of Samba than
   # Ubuntu 18.04 LTS.  See https://alt.fedoraproject.org/cloud/ for
   # info.
+  # If you change to a new distro, update linux_user
   description = "Fedora-Cloud-Base-28-20180922.0.x86_64-hvm-us-east-1-standard-0"
   default = "ami-0064c3021927a1bd5"
 }
