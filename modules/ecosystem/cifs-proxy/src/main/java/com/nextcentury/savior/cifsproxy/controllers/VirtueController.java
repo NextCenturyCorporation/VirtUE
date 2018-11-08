b1eb348e71c0fd1744bd81b83e98ef4231b769b3
Chris Long <chris.long@nextcentury.com>
2018-11-08 11:19:09 -0500

    initial implementation of Virtue POST

---
+++ modules/ecosystem/cifs-proxy/src/main/java/com/nextcentury/savior/cifsproxy/controllers/VirtueController.java
@@ -0,0 +1,38 @@
+package com.nextcentury.savior.cifsproxy.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpSession;
+import javax.xml.ws.WebServiceException;
+
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nextcentury.savior.cifsproxy.model.Virtue;
+import com.nextcentury.savior.cifsproxy.services.VirtueService;
+
+@RestController
+public class VirtueController {
+	private static final XLogger LOGGER = XLoggerFactory.getXLogger(VirtueController.class);
+
+	@Autowired
+	private VirtueService service;
+
+	@PostMapping("/virtue")
+	Virtue newVirtue(HttpSession session, @RequestBody Virtue virtue) {
+		LOGGER.entry(session, virtue);
+		try {
+			service.newVirtue(virtue);
+		} catch (IllegalArgumentException | IOException e) {
+			WebServiceException wse = new WebServiceException("exception while creating a new Virtue", e);
+			LOGGER.throwing(wse);
+			throw wse;
+		}
+		LOGGER.exit(virtue);
+		return virtue;
+	}
+}
