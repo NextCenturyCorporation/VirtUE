1a2214a765c230290d8509f0e4987ebe4e5e5984
Chris Long <chris.long@nextcentury.com>
2018-12-06 15:43:57 -0500

    factored post-deployment configuration out of .tf into shell script

--- modules/ecosystem/cifs-proxy/src/terraform/user-service.tf
+++ modules/ecosystem/cifs-proxy/src/terraform/user-service.tf
@@ -43,48 +43,10 @@ yum -y install \
 	samba-common-tools \
 	samba-dc \
 	sssd
-hostnamectl set-hostname ${local.myname}.${var.domain}
-sed -i 's/\(^127\.0\.0\.1 *\)/\1${local.myname}.${var.domain} ${local.myname} /' /etc/hosts
-(echo supersede domain-name-servers "${local.ds_private_ip}" ';'
-echo supersede domain-search \"${var.domain}\";
-echo supersede domain-name \"${var.domain}\";
-) >> /etc/dhcp/dhclient.conf
-systemctl restart network.service
+
 sed -i 's/^\(\[libdefaults\]\)/\1\n  rdns = false/' /etc/krb5.conf
-echo ${var.admin_password} | realm join --membership-software=samba --user ${var.domain_admin_user} ${var.domain}
-echo Making minimal smb.conf so net ads keytab works
-cp /etc/samba/smb.conf /etc/samba/smb.conf-orig
-(
-echo security = user
-echo realm = ${var.domain}
-echo workgroup = "${local.domain_prefix}"
-echo kerberos method = secrets and keytab
-) | sed -i -e '/^\[global\]$/r /dev/stdin' \
-    -e '/ *\(security\|realm\|workgroup\|kerberos method\) *=/d' \
-	-e '/ *printing *=/,$d' \
-    /etc/samba/smb.conf
-echo '${var.admin_password}' | net -k ads keytab flush -U ${var.domain_admin_user}
-echo '${var.admin_password}' | net -k ads keytab add http -U ${var.domain_admin_user}
-echo '${var.admin_password}' | net -k ads keytab add HTTP -U ${var.domain_admin_user}
 
-echo Configuring Kerberos for our domain
-cat > /etc/krb5.conf.d/savior.conf <<EOCONF
-[libdefaults]
-	default_realm = ${upper(var.domain)}
-	dns_lookup_realm = false
-	dns_lookup_kdc = true
-	forwardable = true
-	proxiable = true
-	default_keytab_name = FILE:/etc/krb5.keytab
-
-[realms]
-	${upper(var.domain)} = {
-		kdc = ${var.domain}
-		admin_server = ${var.domain}
-	}
-EOCONF
-
-echo Setting userPrincipalName and adding permissions for constrained delegation
+echo Adding permissions for constrained delegation
 ldapfile=/tmp/ldap-$$$$
 domainparts=$(echo ${var.domain} | sed 's/\./,dc=/g')
 cat > $ldapfile <<EOLDAP
@@ -94,32 +56,8 @@ add: msDS-AllowedToDelegateTo
 msDS-AllowedToDelegateTo: cifs/${upper(local.fsname)}
 msDS-AllowedToDelegateTo: cifs/${local.fsname}.${var.domain}
 -
-replace: userPrincipalName
-userPrincipalName: http/${local.myname}.${var.domain}@${upper(var.domain)}
--
 EOLDAP
 ldapmodify -f $ldapfile -w '${var.admin_password}' -x -H ldap://${local.ds_private_ip} -D "${var.domain_admin_user}@${var.domain}"
-
-echo fixing reverse dns
-myaddress=$(ip address show dev eth0 | sed -n 's_ *inet \([^/]*\)/.*_\1_p')
-echo '${var.admin_password}' | kinit ${var.domain_admin_user}
-# if the zone doesn't exist, create it
-mysubnet=$(echo $myaddress | sed 's/\.[^.]*$//')
-zone=$(echo $mysubnet | awk -F . '{ print $3 "." $2 "." $1 }').in-addr.arpa
-samba-tool dns zoneinfo ${local.ds_private_ip} $zone \
-	   --kerberos=1 \
-	   >& /dev/null || \
-	samba-tool dns zonecreate ${local.ds_private_ip} $zone \
-		   --kerberos=1
-
-# remove reverse DNS PTR record (if any)
-lastOctet=$${myaddress/*./}
-samba-tool dns delete ${local.ds_private_ip} $zone $lastOctet PTR ${local.myname}.${var.domain} \
-	   --kerberos=1 \
-	   >& /dev/null || true
-# add new reverse DNS PTR record
-samba-tool dns add ${local.ds_private_ip} $zone $lastOctet PTR ${local.myname}.${var.domain} \
-	   --kerberos=1
 
 # create user that will mount files
 useradd --shell /bin/false --no-create-home mounter
@@ -170,19 +108,20 @@ EOF
 	destination = "/tmp/make-virtue-shares.sh"
   }
   
+  provisioner "file" {
+	source = "post-deploy-config.sh"
+	destination = "/tmp/post-deploy-config.sh"
+  }
+  
   provisioner "remote-exec" {
 	inline = [
 	  # install will make them executable by default
-	  "sudo install --target-directory=/usr/local/bin /tmp/${var.import_creds_program} /tmp/${var.switch_principal_program} /tmp/make-virtue-shares.sh"
+	  "sudo install --target-directory=/usr/local/bin /tmp/${var.import_creds_program} /tmp/${var.switch_principal_program} /tmp/make-virtue-shares.sh /tmp/post-deploy-config.sh",
+	  "/usr/local/bin/post-deploy-config.sh --domain ${var.domain} --admin ${var.domain_admin_user} --password ${var.admin_password} --hostname ${local.myname} --dcip ${local.ds_private_ip}"
 	]
-  }
+  }  
 }
 
-#  provisioner "file" {
-#	destination = "/etc/krb5.conf.d/savior.conf"
-#	content =<<EOF
-#EOF
-#  }
   
 # The stuff below sets the userPrincipalName (upn) of the user
 # service.  Without this, kinit doesn't work for the http principal,
