5f17a858e6803e8a9e8e918d8ab78c2bea81a627
Chris Long <chris.long@nextcentury.com>
2019-03-30 12:52:09 -0400

    fixed systemtray behavior on Linux by switching to single click

--- modules/desktop/src/main/java/com/ncc/savior/desktop/sidebar/SidebarApplication.java
+++ modules/desktop/src/main/java/com/ncc/savior/desktop/sidebar/SidebarApplication.java
@@ -1,21 +1,21 @@
 /*
  * Copyright (C) 2019 Next Century Corporation
- * 
+ *
  * This file may be redistributed and/or modified under either the GPL
  * 2.0 or 3-Clause BSD license. In addition, the U.S. Government is
  * granted government purpose rights. For details, see the COPYRIGHT.TXT
  * file at the root of this project.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA.
- * 
+ *
  * SPDX-License-Identifier: (GPL-2.0-only OR BSD-3-Clause)
  */
 package com.ncc.savior.desktop.sidebar;
@@ -52,10 +52,7 @@ import com.ncc.savior.desktop.xpra.protocol.keyboard.SwingKeyMap;
 import com.ncc.savior.desktop.xpra.protocol.keyboard.SwingKeyboard;
 
 /**
- * This is the main class to start just the sidebar application. This may or may
- * not end up being the starting point for the Desktop application.
- *
- *
+ * This is the main class to start the Desktop application.
  */
 public class SidebarApplication {
 	public static void main(String[] args) throws HeadlessException, Exception {
--- modules/desktop/src/main/java/com/ncc/savior/desktop/xpra/application/XpraApplicationManager.java
+++ modules/desktop/src/main/java/com/ncc/savior/desktop/xpra/application/XpraApplicationManager.java
@@ -1,21 +1,21 @@
 /*
  * Copyright (C) 2019 Next Century Corporation
- * 
+ *
  * This file may be redistributed and/or modified under either the GPL
  * 2.0 or 3-Clause BSD license. In addition, the U.S. Government is
  * granted government purpose rights. For details, see the COPYRIGHT.TXT
  * file at the root of this project.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA.
- * 
+ *
  * SPDX-License-Identifier: (GPL-2.0-only OR BSD-3-Clause)
  */
 package com.ncc.savior.desktop.xpra.application;
@@ -112,7 +112,7 @@ public abstract class XpraApplicationManager {
 					onRaiseWindow((RaiseWindowPacket) packet);
 					break;
 				case WINDOW_METADATA:
-					onMetadatPacket((WindowMetadataPacket) packet);
+					onMetadataPacket((WindowMetadataPacket) packet);
 					break;
 				case INITIATE_MOVERESIZE:
 					onInitMoveResize((InitiateMoveResizePacket) packet);
@@ -164,7 +164,7 @@ public abstract class XpraApplicationManager {
 		app.initiateMoveResize(packet);
 	}
 
-	protected void onMetadatPacket(WindowMetadataPacket packet) {
+	protected void onMetadataPacket(WindowMetadataPacket packet) {
 		WindowMetadata meta = packet.getMetadata();
 		XpraApplication app = applications.get(packet.getWindowId());
 		Boolean minimized = meta.getIconicOrNull();
