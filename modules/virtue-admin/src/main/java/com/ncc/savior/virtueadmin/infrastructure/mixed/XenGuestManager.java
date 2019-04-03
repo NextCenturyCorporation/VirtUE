76f6e186ec3d6d51a4f082535cb5093bbe8b913a
Kyle Drumm <kyle.drumm@nextcentury.com>
2019-04-03 12:05:49 -0400

    condensed port forwarding calls

--- modules/virtue-admin/src/main/java/com/ncc/savior/virtueadmin/infrastructure/mixed/XenGuestManager.java
+++ modules/virtue-admin/src/main/java/com/ncc/savior/virtueadmin/infrastructure/mixed/XenGuestManager.java
@@ -288,15 +288,20 @@ public class XenGuestManager {
 		ArrayList<String> lines = new ArrayList<String>();
 		Map<String, Object> dataModel = new HashMap<String, Object>();
 		dataModel.put("guestIp", ipAddress);
-		for (int i = 0; i < numSensingPorts; i++) {
-			dataModel.put("externalPort", externalSensingPort + i);
-			dataModel.put("internalPort", startingInternalPort + i);
-			List<String> o = SshUtil.runScriptFromFile(templateService, session, "port-forwarding.tpl", dataModel);
-			lines.addAll(o);
-			JavaUtil.sleepAndLogInterruption(100);
-		}
+		dataModel.put("externalPort", externalSensingPort );
+		dataModel.put("internalPort", startingInternalPort );
+		dataModel.put("numSensingPorts", numSensingPorts);
+		List<String> o = SshUtil.runScriptFromFile(templateService, session, "port-forwarding.tpl", dataModel);
+		lines.addAll(o);
+//		for (int i = 0; i < numSensingPorts; i++) {
+//			dataModel.put("externalPort", externalSensingPort + i);
+//			dataModel.put("internalPort", startingInternalPort + i);
+//			List<String> o = SshUtil.runScriptFromFile(templateService, session, "port-forwarding.tpl", dataModel);
+//			lines.addAll(o);
+//			JavaUtil.sleepAndLogInterruption(100);
+//		}
 		JavaUtil.sleepAndLogInterruption(100);
-		logger.debug(lines.toString());
+		logger.debug(lines.toString() );
 	}
 
 	private String getIpFromConsole(CommandHandler ch, String name) {
