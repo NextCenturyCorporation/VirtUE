748043962a675315b6f8d25a1617a44ca8b88b32
Chris Long <chris.long@nextcentury.com>
2019-04-25 15:19:02 -0400

    improved thread safety

--- modules/desktop/src/main/java/com/ncc/savior/desktop/xpra/XpraClient.java
+++ modules/desktop/src/main/java/com/ncc/savior/desktop/xpra/XpraClient.java
@@ -1,21 +1,21 @@
 /*
  * Copyright (C) 2019 Next Century Corporation
- * 
+ *
  * This file may be redistributed and/or modified under either the GPL
  * 2.0 or 3-Clause BSD license. In addition, the U.S. Government is
  * granted government purpose rights. For details, see the COPYRIGHT.TXT
  * file at the root of this project.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA.
- * 
+ *
  * SPDX-License-Identifier: (GPL-2.0-only OR BSD-3-Clause)
  */
 package com.ncc.savior.desktop.xpra;
@@ -76,7 +76,7 @@ public class XpraClient implements Closeable {
 	// private IConnection connection;
 	private IKeyboard keyboard;
 	protected InputStreamPacketReader packetReader;
-	protected boolean stopReadThread;
+	protected volatile boolean stopReadThread;
 	private static int threadCount = 1;
 	private Status status;
 	private int display;
@@ -88,7 +88,6 @@ public class XpraClient implements Closeable {
 		packetReceivedListenerManager = new PacketListenerManager();
 		packetSentListenerManager = new PacketListenerManager();
 		status = Status.DISCONNECTED;
-
 
 		internalPacketDistributer.addPacketHandler(PacketType.HELLO, new BasePacketHandler(PacketType.HELLO) {
 
@@ -161,12 +160,14 @@ public class XpraClient implements Closeable {
 						in = connection.getInputStream();
 						packetReader = new InputStreamPacketReader(in, new PacketBuilder());
 						Packet packet = null;
-						while ((packet = packetReader.getNextPacket()) != null && !stopReadThread) {
+						while (!stopReadThread && (packet = packetReader.getNextPacket()) != null) {
 							internalPacketDistributer.handlePacket(packet);
 							packetReceivedListenerManager.handlePacket(packet);
 						}
 
 					} catch (IOException e) {
+						logger.debug("error reading packets from Xpra connection to "
+								+ connection.getConnectionParameters().getConnectionKey());
 						onIoException(e);
 					}
 				}
